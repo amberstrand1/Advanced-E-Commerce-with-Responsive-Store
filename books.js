e2630074b8dd89c5d1560cec40ab09bef0e1714b
amberspake1 <amberspake1@gmail.com>
2026-01-15 14:52:09 -0800

    Updates to books.html and javascript

--- books.js
+++ books.js
@@ -1,26 +1,161 @@
-function renderBooks() {
-const booksWrapper = document.querySelector('.books');
-booksWrapper.innerHTML = 
-'<div class="book">
+function renderBooks(filter) {
+const booksWrapper = document.querySelector(".books")
+
+const books = getBooks();
+
+    if (filter === 'LOW_TO_HIGH') {
+       books.sort((a, b) => a.originalPrice - b.originalPrice);
+    }
+    else if (filter === 'HIGH_TO_LOW') {
+       books.sort((a, b) => b.originalPrice - a.originalPrice);
+    }
+    else if (filter === 'RATING') {
+       books.sort((a, b) => b.rating - a.rating);
+    }
+
+const booksHtml = books.map((book) => {
+  return  `<div class="book">
 <figure class="book__img--wrapper">
-    <img class="book__img" src="atomic habits.jpg" alt="">
+    <img class="book__img" src="${book.url}" alt=""/>
 </figure>
 <div class="book__title">
-    Atomic Habits
+    ${book.title}
 </div>
 <div class="book__ratings">
- <i class="fa-solid fa-star"></i>
- <i class="fa-solid fa-star"></i>
- <i class="fa-solid fa-star"></i>
- <i class="fa-solid fa-star"></i>
- <i class="fa-solid fa-star-half"></i>
- </div>
- <div class="book__price">
- <span class="book__price--normal">$59.95</span> $14.95
- </div>
-</div>'
+    ${ratingsHtml(book.rating)}
+    </div>
+    <div class="book__price">
+    <span>$${book.originalPrice.toFixed(2)}</span>
+    </div>
+    </div>`
+})
+
+.join("")
+
+booksWrapper.innerHTML = booksHtml;
 }
+
+function ratingsHtml(rating) {
+    let ratingHtml = '';
+
+    for (let i = 0; i < Math.floor(rating); i++) {
+        ratingHtml += '<i class="fa-solid fa-star"></i>';
+    }
+
+    if (!Number.isInteger(rating)) {
+        ratingHtml += '<i class="fa-solid fa-star-half"></i>';
+    }
+
+    return ratingHtml;
+}
+
+function filterBooks(event) {
+    if (event.target.value === 'LOW_TO_HIGH');
+}
+
 
 setTimeout(() => {
 renderBooks();
 })
+
+function getBooks() {
+    return [
+    {
+        id: 1,
+        title: "Cracking the Coding Interview",
+        url: "crack-the-coding-interview.png",
+        originalPrice: 59.95,
+        salePrice: 14.95,
+        rating: 4.5,
+    },
+    {
+        id: 2,
+        title: "Atomic Habits",
+        url: "atomic-habits.jpg",
+        originalPrice: 59.95,
+        salePrice: 13.95,
+        rating: 4.5,
+    },
+    {
+        id: 3,
+        title: "Can't Hurt Me",
+        url: "david-goggins.jpeg",
+        originalPrice: 59.95,
+        salePrice: 15.00,
+        rating: 4.5,
+    },
+    {
+        id: 4,
+        title: "Deep Work",
+        url: "deep-work.jpeg",
+        originalPrice: 59.95,
+        salePrice: 12.00,
+        rating: 4.5,
+    },
+    {
+        id: 5,
+        title: "The 10X Rule",
+        url: "book-1.jpeg",
+        originalPrice: 59.95,
+        salePrice: 13.00,
+        rating: 4.5,
+    },
+    {
+        id: 6,
+        title: "Be Obsessed or Be Average",
+        url: "book-2.jpeg",
+        originalPrice: 59.95,
+        salePrice: 11.95,
+        rating: 4.5,
+    },
+    {
+        id: 7,
+        title: "Rich Dad Poor Dad",
+        url: "book-3.jpeg",
+        originalPrice: 59.95,
+        salePrice: 10.95,
+        rating: 4.5,
+    },
+    {
+        id: 8,
+        title: "Cashflow Quadrant",
+        url: "book-4.jpeg",
+        originalPrice: 59.95,
+        salePrice: 17.95,
+        rating: 4.5,
+    },
+    {
+        id: 9,
+        title: "Psychology of Money",
+        url: '"book-5.jpeg',
+        originalPrice: 59.95,
+        salePrice: 16.95,
+        rating: 4.5,
+    },
+    {
+        id: 10,
+        title: "The 5 Seconds Rule",
+        url: "book-6.jpeg",
+        originalPrice: 59.95,
+        salePrice: 16.00,
+        rating: 4.5,
+    },
+    {
+        id: 11,
+        title: "Your Next Five Moves",
+        url: "book-7.jpg",
+        originalPrice: 59.95,
+        salePrice: 21.99,
+        rating: 4.5,
+    },
+    {
+        id: 12,
+        title: "Mastery",
+        url: "book-8.jpeg",
+        originalPrice: 59.95,
+        salePrice: 20.00,
+        rating: 4.5,
+    }
+    
+    ]
+}
